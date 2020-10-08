c19ee1d35c1c5aa6887bbca933c0a8ae48ac1375
Nellis,Dean <dean.nellis@lexmark.com>
2020-10-08 14:12:29 -0400

    added favicon, updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Dean Nellis Prtfolio Site`,
+        short_name: `Dean Nellis`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#05AFF2`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
