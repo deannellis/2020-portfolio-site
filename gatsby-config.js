682775f68a3e1ca70e5d82616bf2084e92b93e02
Nellis,Dean <dean.nellis@lexmark.com>
2020-10-21 09:46:29 -0400

    Troubleshooting Netlify deploy error

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
